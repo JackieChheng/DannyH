78ccd074cf9a284191c7ba49e242ec424cdd122f
JackieChheng <jackiechheng4@gmail.com>
2024-07-15 17:56:10 -0600

    changed scrolling and added ids

--- script.js
+++
@@ -1,6 +0,0 @@
-var $scroll = $('.sidebar');
-$('.nav-item > a').each(function () {
-   if ($(this).hasClass('active')) {
-      $scroll.scrollTop($(this).position().top + $scroll.scrollTop())
-   }
-})
